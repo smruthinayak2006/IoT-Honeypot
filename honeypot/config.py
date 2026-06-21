8223488b7954edda0a9905dca70a5e14e5414855
Smruthi Nayak <smritinayak2006@gmail.com>
2026-06-21 08:53:31 +0530

    Added environment based configuration

--- honeypot/config.py
+++ honeypot/config.py
@@ -1,28 +1,55 @@
 """
-Honeypot Configuration
+Central Configuration Management
 """
 
+import os
 
-# Server Configuration
-
-HOST = "0.0.0.0"
-
-PORT = 2323
+from dotenv import load_dotenv
 
 
-
-# Database Configuration
-
-DATABASE_PATH = "database/attacks.db"
+load_dotenv()
 
 
+HOST = os.getenv(
+    "HONEYPOT_HOST",
+    "0.0.0.0"
+)
 
-# Security Configuration
 
-BRUTE_FORCE_LIMIT = 5
+PORT = int(
+    os.getenv(
+        "HONEYPOT_PORT",
+        2323
+    )
+)
 
 
+DATABASE_PATH = os.getenv(
+    "DATABASE_PATH",
+    "database/attacks.db"
+)
 
-# Report Configuration
 
-REPORT_FOLDER = "reports"
+BRUTE_FORCE_LIMIT = int(
+    os.getenv(
+        "BRUTE_FORCE_LIMIT",
+        5
+    )
+)
+
+
+SECURITY_LOG_PATH = os.getenv(
+    "SECURITY_LOG_PATH",
+    "logs/security.log"
+)
+
+
+ATTACK_LOG_PATH = os.getenv(
+    "ATTACK_LOG_PATH",
+    "logs/attack_logs.txt"
+)
+
+REPORT_FOLDER = os.getenv(
+    "REPORT_FOLDER",
+    "reports"
+)
